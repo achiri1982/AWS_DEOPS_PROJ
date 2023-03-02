cc73851d72abddbcbbfa1e562f076b68680aa0af
Leslie foryoung <104910595+achiri1982@users.noreply.github.com>
2023-03-02 00:19:48 -0500

    Build jenkins instance static ip

--- userdata.sh
+++ userdata.sh
@@ -4,7 +4,13 @@
 sudo yum update -y
 sudo yum -y install docker
 sudo systemctl enable docker
-sudo sytemctl start docker
+sudo systemctl start docker
 
 #Install Jenkin
 sudo docker run -d -p 8080:8080 -p 5000:5000 -v jenkins_home:/var/jenkins_home jenkins/jenkins:lts-jdk11
+
+sudo docker ps -a | grep Up
+if echo $? > 0; then
+    sudo systemctl start docker
+    sudo docker start krazy_kare
+fi
